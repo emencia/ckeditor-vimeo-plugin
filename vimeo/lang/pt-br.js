cb24ed85287c10a60a9f80db62bce646b56e9049
Mauricio Eugênio <mauricio@digitalbluemarketing.com.br>
2018-09-21 10:27:08 -0300

    translation for portuguese brazilian - #vaiBrasiliam

---
+++ vimeo/lang/pt-br.js
@@ -0,0 +1,18 @@
+CKEDITOR.plugins.setLang('vimeo', 'pt-br', {
+    button : 'Inserir Vimeo Video',
+    title : 'Embed Vimeo Video',
+    txtEmbed : 'Cole o código do embed',
+    txtUrl : 'Cole URL do video vimeo',
+    txtWidth : 'Largura',
+    txtHeight : 'Altura',
+    chkAutoplay : 'Autoplay',
+    noCode : 'Você deve inserir o código ou URL do video',
+    invalidEmbed : 'O código do embed inserido não é válido',
+    invalidUrl : 'O URL não é válido',
+    or : 'ou',
+    noWidth : 'Você deve informar a largura',
+    invalidWidth : 'Informe uma largura válida',
+    noHeight : 'Você deve informar a altura',
+    invalidHeight : 'Informe uma altura válida',
+    txtResponsive : 'Deixar responsivo (ignore largura e altura, ajuste à largura)'
+});
